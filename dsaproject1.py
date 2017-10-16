51d812412401d8e7464b35d4f71dfc0565567f41
Akiri Prabhanda <31247199+Prabhanda-Akiri@users.noreply.github.com>
2017-10-16 19:55:59 +0530

    Update dsaproject1.py

--- dsaproject1.py
+++ dsaproject1.py
@@ -1,6 +1,6 @@
 class Node:
 	
-	def __init__(self):
+	def __init__(self):			#attributes of tree nodes
 
 		self.parent=None
 		self.left=None
@@ -14,8 +14,8 @@ class SegmentTree:
 
 	def __init__(self,n):
 
-		self.n=n-1
-		self.root=None
+		self.n=n-1			#pointer to keep the trace of total elements
+		self.root=None		
 
 	def design(self,s,e):
 
@@ -25,14 +25,16 @@ class SegmentTree:
 		x.end=e
 
 		if e==self.n and s==0:
-			self.root=x
 
+			self.root=x									#assigning the root if the tree is empty
 
 		if s==e:
 			return x
 
 		else:
+
 			m=int((s+e)/2)
+
 			p=self.design(s,m)
 			q=self.design(m+1,e)
 
@@ -48,14 +50,17 @@ class SegmentTree:
 
 		if x==None:
 			return
+
 		if x.start==i and x.end==j:
 			return x
 
-		else:
+		else:									#recursive search
 			p=self.sear(x.left,i,j)
 			q=self.sear(x.right,i,j)
+
 			if p!=None:
 				return p
+
 			if q!=None:
 				return q
 				
@@ -66,16 +71,19 @@ class SegmentTree:
 
 		for i in range(0,n):
 
-			t=self.sear(self.root,i,i)
+			t=self.sear(self.root,i,i)			#searching the right leaf to insert the array element
 			t.val=a[i]
 
-	def summing(self):
+	def summing(self):					
 
-		for i in range(0,self.n+1):
+		for i in range(0,self.n+1):					#adding the values of both the children and storing that value in the parent
+
 			for j in range(i+1,self.n+1):
 
 				for p in range(j-1,-1,-1):
+
 					t=self.sear(self.root,p,j)
+
 					if t!=None:
 						t.val=t.right.val+t.left.val
 
@@ -85,18 +93,22 @@ class SegmentTree:
 					t.val=t.right.val+t.left.val
 
 				k=self.n-j
+
 				t=self.sear(self.root,k,self.n)
 
 				if t!=None:
+
 					t.val=t.right.val+t.left.val
 
-	def traversal(self,x):
-
+	def traversal(self,x):			
+										#enables us to verify if the values are managed properly in the tree
 		if x==None:
 			return
 
 		else:
+
 			print(x.val)
+
 			self.traversal(x.left)
 			self.traversal(x.right)
 
@@ -106,11 +118,13 @@ class SegmentTree:
 			return
 		else:
 			self.intraversal(x.left)
+
 			print(x.val)
+
 			self.intraversal(x.right)
 
 	def rangesum(self,x,s,e):
-
+										#gives the sum of prescribed range in the array
 		if s==x.start and e==x.end:
 
 			return x.val
@@ -121,15 +135,22 @@ class SegmentTree:
 			q=0
 
 			if x.left!=None:
+
 				if x.left.end>=s:
+
 					if e<x.left.end:
 						q=self.rangesum(x.left,s,e)
+
 					else:
 						q=self.rangesum(x.left,s,x.left.end)
+
 			if x.right!=None:
+
 				if x.right.start<=e:
+
 					if s>x.right.start:
 						p=self.rangesum(x.right,s,e)
+
 					else:
 						p=self.rangesum(x.right,x.right.start,e)
 			
@@ -143,11 +164,11 @@ class SegmentTree:
 			if x.left!=None:
 				if x.left.end>=s:
 					q=self.rangesum(x.left,s,x.left.end)
+
 			if x.right!=None:
 				if x.right.start<=e:
 					p=self.rangesum(x.right,x.right.start,e)
 			
-
 			return p+q
 
 		else:
@@ -155,12 +176,16 @@ class SegmentTree:
 			return 0
 
 	def update(self,i,n):
+									#updates the array element from initial value to the new value n
 
 		t=self.sear(self.root,i,i)
+
 		k=n-t.val
+
 		self.add(self.root,t.start,t.end,k)
 
-	def add(self,x,s,e,k):
+	def add(self,x,s,e,k):						
+									#adds the extra value to the elements in the tree when it's child value is updated
 
 		if s==x.start and e==x.end:
 
@@ -171,17 +196,22 @@ class SegmentTree:
 
 			if x.left!=None:
 				if x.left.end>=s:
+
 					if e<x.left.end:
 						x.val=x.val+k
 						self.add(x.left,s,e,k)
+
 					else:
 						x.val=x.val+k
 						self.add(x.left,s,x.left.end,k)
+
 			if x.right!=None:
 				if x.right.start<=e:
+
 					if s>x.right.start:
 						x.val=x.val+k
 						self.add(x.right,s,e,k)
+
 					else:
 						x.val=x.val+k
 						self.add(x.right,x.right.start,e,k)
@@ -190,17 +220,22 @@ class SegmentTree:
 
 			if x.left!=None:
 				if x.left.end>=s:
+
 					if e<x.left.end:
 						x.val=x.val+k
 						self.add(x.left,s,e,k)
+
 					else:
 						x.val=x.val+k
 						self.add(x.left,s,x.left.end,k)
+
 			if x.right!=None:
 				if x.right.start<=e:
+
 					if s>x.right.start:
 						x.val=x.val+k
 						self.add(x.right,s,e,k)
+
 					else:
 						x.val=x.val+k
 						self.add(x.right,x.right.start,e,k)
@@ -221,6 +256,7 @@ class TrainSchedules:
 		self.no=None
 
 	def build(self):
+						#builds two segment trees for each attribute time and distance
 
 		self.T.time.design(0,8)
 		self.T.time.insert(self.times)
@@ -235,15 +271,19 @@ class TrainSchedules:
 
 		
 
-		print('Train Name:		',self.name,'\n')
+		print('\nTrain Name:		',self.name,'\n')
 		
 		print('Train Number:		',self.no,'\n')
 		print('Starting Station:	',self.Stops[0])
 		print('Final Station:		',self.Stops[-1],'\n')
+
 		print('Route List:\n')
+
 		n=len(self.Stops)
+
 		for i in range(0,n):
 			print(i+1,'.',self.Stops[i])
+
 		print('\n')
 
 	def Time(self,i,j):
@@ -252,19 +292,25 @@ class TrainSchedules:
 		return s
 
 	def Distance(self,i,j):
+
 		s=self.T.distance.rangesum(self.T.distance.root,i,j-1)
 		return s
 
 	def upTime(self,n,k):
+
 		self.T.time.update(n,k)
 
 	def upDistance(self,n,k):
+
 		self.T.distance.update(n,k)
+
+
 	
 class tr:
 
 	def __init__(self):
 
+							#creates two attributes to the Train Schedules objects...
 		self.time=None
 		self.distance=None
 
@@ -272,11 +318,15 @@ class tr:
 def main():
 
 	n=2
+
 	names=['Mangalore Central Malabar Express','Poorna Express		']
 	nums=['16630','11097']
-	Train=[]
-	for i in range(0,2):
 
+	Train=[]
+
+	for i in range(0,n):
+
+											#creates TrainSchedule objects for all the trains
 		Train.append(TrainSchedules())
 		Train[i].name=names[i]
 		Train[i].no=nums[i]
@@ -293,84 +343,169 @@ def main():
 
 
 	print('\n\n*********** Welcome to South Western Railway Zone ************\n')
+
 	print('Trains provided :\n\n    Train name          			Train Number\n')
 
 	for i in range(0,n):
 		print(i+1,'.',names[i],'		',nums[i])
 
-	c=int(input('\nSelect Your Train\n'))-1
-
-	Train[c].intro()
 	while 1:
 
-		k=int(input('Choose Your Module:\n\n1.Passenger Module\n2.Administrative Module\n3.Exit\n'))
+		c=int(input('\nSelect Your Train\n'))-1
+
+		if c<0 or c>n-1:
+			print('Enter a proper choice:')
+
+		else:
+			break
+
+	Train[c].intro()
+
+
+	while 1:
+
+		k=int(input('\nChoose Your Module:\n\n1.Passenger Module\n2.Administrative Module\n3.Exit\n\n'))
 
 		if k==1:
 
+						#passenger module
+
+			print('\n\nWelcome to the customer..\n\nNow you are accessed to choose boarding point and destination point, then check journey time and journey distance multiple times\nFinally book your ticket..!!\n')
+
 			while 1:
 
-				s=int(input('Enter the starting station.no:	'))
-				e=int(input('Enter the ending station.no:	'))
+
+				s=int(input('\nEnter the boarding station.no:	'))
+				e=int(input('Enter the destination station.no:	'))
 
 				h=-1
 
 				t=Train[c].Time(s-1,e-1)
 				d=Train[c].Distance(s-1,e-1)
+				
 				m=t%60
 				h=int(t/60)
 
 				if h>0:
-					print('\nThe Train takes ',h,' Hours ',m,' Minutes, ',d,'kms\n')
+					print('\nThe Train takes ',h,' Hours ',m,' Minutes to travel ',d,'kms\n')
 				else:
 					print('\nThe Train takes ',m,' Minutes to travel ',d,'kms\n')
 				p=0
-				p=int(input('\nDo you want to take a ticket and exit the Passenger Module?\n1.Yes\n2.No\n'))
+
+				p=int(input('\nDo you want to take a ticket and exit the Passenger Module?\n\n1.Yes\n2.No\n\n'))
+
 				if p==1:
-					f=int(input('Enter number of people:'))
-					r=(0.5*d)+(0.90*t)
-					print('\n\n 	___________________________________________')
+
+					f=int(input('\nEnter number of Passengers:	'))
+
+					r=(1.5*d)*f 							#bill calculation
+
+					print('\n\n 	_________________________________________________')
 					print('\n\n	*******************************************	\n')
+
 					print('	Starting point:	',Train[c].Stops[s-1],'					')
 					print('	Ending point:	',Train[c].Stops[e-1],'					\n\n')
-					print('	Your intermediate stations:				\n')
-					for i in range(s,e-1):
-						print('	',Train[c].Stops[i],'				')
+
+					if h>0:
+						print('	Journey Time:	',h,' Hours ',m,' Minutes \n\n')
+					else:
+						print('	Journey Time:	',m,' Minutes\n\n')
+
+					print('	Journey distance:	',d,' kms\n\n')
+
+					if s+1==e:
+						print('You don\'t have any intermediate stations\n')
+
+					else:
+						print('	Your intermediate stations:				\n')
+
+						for i in range(s,e-1):
+							print('	',Train[c].Stops[i],'				')
+
 					print('\n	Bill:			Rs.',r,'	')
+
 					print('\n	**********************************	\n\n')
 					print('	Thank You for using the South Western Railways\n\n        ***** Have a Happy Journey *****\n')
-					print('\n 	___________________________________________\n')
+					print('\n 	_________________________________________________\n')
 
 					break
 
 		elif k==2:
 
+												#administrative module..
+
 			while 1:
 
-				p=int(input('\nChoose your operation:\n1.Update delay timings at a station\n2.Change a stop\n'))
+				p=int(input('\nChoose your operation:\n1.Update delay timings at a station\n2.Change a stop\n\n'))
 				if p==1:
 
-					s=int(input('enter the station number'))
-					u=int(input('enter the new travel time'))
+					s=int(input('\nenter the station number at which delay occured:		'))
+					u=int(input('enter the new travel time to reach the next station:	'))
+
 					Train[c].upTime(s-1,u)
 
-				if p==2:
+					t=int(input('\nYour changes are updated..!!!\n\nDo you still want to continue in the Administrative Module and update some changes?:\n\n1.Yes\n2.No\n'))
 
-					s=int(input('enter the station number:	'))
+					if t==2:
+
+						print('\n***Railways offer sincere regards for your service***\n\n        Have a nice Day...!!!!\n\n')
+
+						break
+
+
+				elif p==2:
+
+					s=int(input('\nenter the station number:	'))
+
 					na=input('enter new station name:		')
-					di=int(input('enter new station distance:	'))
-					u=int(input('enter the new travel time:	'))
+
+					di=int(input('\nenter distance to be travelled to reach the new station:		'))
+					de=int(input('enter distance to be travelled from new station to next station:	'))
+
+					u=int(input('\nenter time to be travelled to reach the new station (in mins):		'))
+					v=int(input('enter time to be travelled from new station to next station(in mins):	'))
+
 					Train[c].Stops[s-1]=na
-					Train[c].upDistance(s-1,di)
-					Train[c].upTime(s-1,u)
 
-				t=int(input('\nDo you still want to continue in the Administrative Module and update some changes?:\n1.Yes\n2.No\n'))
+					Train[c].upDistance(s-2,di)
+					Train[c].upDistance(s-1,de)
 
-				if t==2:
-					print('\n***Railways offer sincere regards for your service***\n\n        Have a nice Day...!!!!\n\n')
-					break
+					Train[c].upTime(s-2,u)
+					Train[c].upTime(s-1,v)
+
+					t=-1
+					print('\nYour changes are updated..!!!\n\nDo you still want to continue in the Administrative Module and update some changes?:\n\n1.Yes\n2.No\n')
+
+					while 1:
+
+						t=int(input('\nEnter your Choice\n'))
+
+						if t<1 or t>2:
+							print('\nInvalid choice..!!')
+
+						else:
+							break
+
+					if t==2:
+
+						print('\n***Railways offer sincere regards for your service***\n\n        Have a nice Day...!!!!\n\n')
+
+						break
+
+				else:
+
+					print('\nSelect a proper case...\n')
+
 		elif k==3:
+							#exit case
+
 			print('		South Western Railways - Always at people\'s service....!!!!')
 			break
+
+		else:
+					#invalid case
+
+			print('Select a proper case....')
 
 
 
