b21c6d78d66eca07984a3e32ec4ec2bc8aaf4107
Akiri Prabhanda <31247199+Prabhanda-Akiri@users.noreply.github.com>
2017-11-12 02:08:01 +0530

    Update AdminModule.java

--- AdminModule.java
+++ AdminModule.java
@@ -1,5 +1,6 @@
-package ProjectAdmin;
+package Project;
 import java.util.Scanner;
+import ProjectFlight.*;
 
 interface Admin
 {
@@ -9,7 +10,6 @@ interface Admin
 	public void ChangePassword();
 
 	public void AddAirline();
-	public void StopAirline();
 
 	public void ChangeFare();
 	public void DailyTransaction();
@@ -17,29 +17,147 @@ interface Admin
 
 abstract class AdminAccount implements Admin
 {
+	String FirstName,LastName;
+	String PhoneNo;
+	String email;
+	String UserName,Password;
+	
+	Scanner in=new Scanner(System.in);
+	Scanner s=new Scanner(System.in);
 	public boolean CreateAccount()
 	{
-		System.out.println("pass");
+		int n;
+		System.out.println("PLEASE ENTER YOUR VALID DETAILS TO CREATE AN ACCOUNT .......\n\n");
+		System.out.print("FIRST NAME: ");
+		FirstName=s.next();
 
-		return true;
+		System.out.print("LAST NAME: ");
+		LastName=s.next();
+		System.out.println("\n");
+		try
+		{
+			System.out.print("PHONE NUMBER: ");
+			PhoneNo=s.next();
+			System.out.println("\n");
+			if (PhoneNo.length()<10)
+			{
+				throw new ArithmeticException("SHOULD CONTAIN 10 DIGIT NUMBER!!!\nPLEASE ENTER A VALID PHONE NUMBER:");
+			}
+
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.print("Error:"+e.getMessage());
+			PhoneNo=s.next();
+			System.out.println("\n");
+		}
+
+		finally
+		{
+			System.out.print("EMAIL ID : ");
+			email=s.next();
+			System.out.println("\n");
+
+			System.out.print("PLEASE ENTER A VALID USERNAME: ");
+			UserName=s.next();
+			System.out.println("\n");
+
+			System.out.print("PLEASE ENTER A 8 DIGIT PASSWORD:");
+			Password=s.next();
+			System.out.println("\n");
+
+			System.out.println("CONGRAGULATIONS!!!!.......YOUR ACCOUNT HAS BEEN SUCCESSFULLY CREATED\n\n");
+			System.out.print("Do u want to continue?\n Please enter (1 for YES/2 for NO):");
+			n=s.nextInt();
+			if(n==1)
+				return true;
+			else
+				return false;
+
+		}
 	}
 
 	public boolean LoginAccount()
 	{
-		System.out.println("pass");
+		int i=0;
+		String name,pwd;
 
-		return true;
+		while(i<=2)
+		{
+			System.out.print("ENTER THE USERNAME: ");
+			name=s.next();
+			System.out.println("\n\n");
+			System.out.print("ENTER THE PASSWORD: ");
+			pwd=s.next();
+			if(name.equals(UserName) && pwd.equals(Password))
+			{
+				System.out.println("Successfully logged in!!!\n\n");
+				return true;
+			}
+			else
+
+				System.out.println("INCORRECT USERNAME OR PASSWORD!!!....PLEASE TRY AGAIN!!! "+(int)(2-i)+" CHANCES ARE LEFT ");
+
+			i=i+1;
+		}
+
+		System.out.println("SORRY NUMBER OF TRIALS OVER!!!");
+		return false;
 	}
 
 	public void LogoutAccount()
 	{
-		System.out.println("pass");
+		System.out.println("You have been successfully logged out");
 	}
 
 	public void ChangePassword()
 	{
-		System.out.println("pass");
+		String Passwd;
+     	String Passwd2;
+     	int t1=2;
+     	int t2=3;
+     
+    	System.out.println("Your password must contain minimum 8 characters");
+    	System.out.println("Atleast one character must be numeric");
+    	System.out.println("Enter your new password:");
+     
+     	while(t1>1)
+     	{
+  			Passwd=in.next();
+  
+  			int length=Passwd.length();
+  
+  			if(length>=8)
+  			{
+  
+     			System.out.println("Re-enter your password:");
+      			Passwd2=in.next();
+      			while(t2>1)
+      			{
+          
+          			if(Passwd.equals(Passwd2))
+          			{
+              			System.out.println("Password successfully changed");
+              			Password=Passwd;
+              			t2=0;
+              			t1=0;
+          			}
+          			else
+          			{
+              			System.out.println("Re-enter your password");
+          			}
+      			}
+  			}
+
+  			else
+  			{
+      			System.out.println("Please enter a valid password:");
+      
+  			}
+  
+     	}
 	}
+	
 }
 
 
@@ -47,25 +165,41 @@ abstract class AdminAbstract extends AdminAccount
 {
 	public void AddAirline()
 	{
-		System.out.println("pass");
+		ProjectFlight.Flights F1=new ProjectFlight.Flights();
+
+		F1.Add();
+
 	}
 
-	public void StopAirline()
-	{
-		System.out.println("pass");
-	}
 }
 
 public class AdminModule extends AdminAbstract
 {
 	public void ChangeFare()
 	{
-		System.out.println("pass");
+		ProjectFlight.Flights ob=new ProjectFlight.Flights();
+
+		System.out.println("Here are the details of the Flights provided");
+		ob.show();
+
+		Scanner in=new Scanner(System.in);
+		System.out.println("Choose the Flight to change the Fare..\n");
+		int n=in.nextInt();
+		System.out.println("Enter your new Fare..\n");
+		double f=in.nextDouble();
+
+		ob.Fls[n].Fare=f;
+
+		System.out.println("Your New fare is updated\n");
+
+		ob.show();
 	}
 
 	public void DailyTransaction()
 	{
-		System.out.println("pass");
+		ProjectFlight.Flights ob=new ProjectFlight.Flights();
+
+		System.out.println("Todays Daily Transactions are\n"+"						Rs."+ob.transaction+"/- only....");
 	}
 }
 
