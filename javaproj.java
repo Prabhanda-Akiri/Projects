3037006f050eb4c781222e59f53a430bb296b9a4
Akiri Prabhanda <31247199+Prabhanda-Akiri@users.noreply.github.com>
2017-11-11 18:54:10 +0530

    Update javaproj.java

--- javaproj.java
+++ javaproj.java
@@ -1,128 +1,9 @@
 import java.util.Scanner;
 
-interface User
-{
-	public void CreateAccount();
-	public boolean LoginAccount();
+import ProjectUser.*;
 
-	public void BookTickets();
-	public void ChangePassword();
+import ProjectAdmin.*;
 
-	public void CancelTickets();
-	public void FlightDetails();
-	public void LogoutAccount();
-}
-
-abstract class UserAbstract implements User
-{
-	public void CreateAccount()
-	{
-		System.out.println("pass");
-	}
-
-	public boolean LoginAccount()
-	{
-		System.out.println("pass");
-
-		return true;
-	}
-
-	public void ChangePassword()
-	{
-		System.out.println("pass");
-	}
-}
-
-class UserModule extends UserAbstract
-
-{
-	public void BookTickets()
-	{
-		System.out.println("pass");
-	}
-
-	public void CancelTickets()
-	{
-		System.out.println("pass");
-	}
-
-	public void FlightDetails()
-	{
-		System.out.println("pass");
-	}
-
-	public void LogoutAccount()
-	{
-		System.out.println("pass");
-	}
-}
-
-
-interface Admin
-{
-	public void CreateAccount();
-	public boolean LoginAccount();
-	public void LogoutAccount();
-	public void ChangePassword();
-
-	public void AddAirline();
-	public void StopAirline();
-
-	public void ChangeFare();
-	public void DailyTransaction();
-}
-
-abstract class AdminAccount implements Admin
-{
-	public void CreateAccount()
-	{
-		System.out.println("pass");
-	}
-
-	public boolean LoginAccount()
-	{
-		System.out.println("pass");
-
-		return true;
-	}
-
-	public void LogoutAccount()
-	{
-		System.out.println("pass");
-	}
-
-	public void ChangePassword()
-	{
-		System.out.println("pass");
-	}
-}
-
-
-abstract class AdminAbstract extends AdminAccount
-{
-	public void AddAirline()
-	{
-		System.out.println("pass");
-	}
-
-	public void StopAirline()
-	{
-		System.out.println("pass");
-	}
-}
-
-class AdminModule extends AdminAbstract
-{
-	public void ChangeFare()
-	{
-		System.out.println("pass");
-	}
-
-	public void DailyTransaction()
-	{
-		System.out.println("pass");
-	}
-}
 
 
 public class javaproj
@@ -156,7 +37,7 @@ public class javaproj
 		{
 			System.out.println("\n\nWelcome to the User-Module..!!\n");
 
-			UserModule user=new UserModule();
+			ProjectUser.UserModule user=new ProjectUser.UserModule();
 
 			System.out.println("1.Not yet Registered??	Make an account and start enjoying the services of SPICE JET....\n");
 			System.out.println("2.Already a member of the SPICE JET family??	Login and check the flight details..\n");
@@ -177,7 +58,10 @@ public class javaproj
 
 			if(UserStatus==1)
 			{
-				user.CreateAccount();
+				boolean t;
+				t=user.CreateAccount();
+				if(t==true)
+					UserStatus=2;
 			}
 
 			if(UserStatus==2)
@@ -190,8 +74,9 @@ public class javaproj
 				if(LoginDetails==true)
 				{
 					System.out.println("You are properly logged into your account..\n\nHere are the services provided to your module..\n");
+				while(2>1)
 
-					System.out.print("1.Book your tickets\n2.Cancel your already booked tickets\n");
+				{	System.out.print("\n1.Book your tickets\n2.Cancel your already booked tickets\n");
 					System.out.println("3.Search for the Flights available on different dates(in case you don't want to book your ticket now)");
 					System.out.println("4.Logout and Exit the User Module");
 					System.out.println("\nChoose your service..");
@@ -211,28 +96,29 @@ public class javaproj
 					if(UserServices==1)
 					{
 						user.BookTickets();
-						System.out.println("\nThank You for using SPICE JET....!!!\n");
-						System.out.println("Have a Great Day..!!\n");
+						
 					}
 
 					else if(UserServices==2)
 					{
 						user.CancelTickets();
-						System.out.println("\nThank You for using SPICE JET....!!!\n");
-						System.out.println("Have a Great Day..!!\n");
+						
 					}
 
 					else if(UserServices==3)
 					{
 						user.FlightDetails();
-						System.out.println("\nThank You for using SPICE JET....!!!\n");
-						System.out.println("Have a Great Day..!!\n");
+						
 					}
 
 					else
 					{
 						user.LogoutAccount();
+						System.out.println("\nThank You for using SPICE JET....!!!\n");
+						System.out.println("Have a Great Day..!!\n");
+						break;
 					}
+				}
 
 				}
 
@@ -269,11 +155,11 @@ public class javaproj
 
 		}
 
-		else if(ModuleSelection==2)
+		if(ModuleSelection==2)
 		{
 			System.out.println("\n\nWelcome to the Administrative Module...!!");
 
-			AdminModule admin=new AdminModule();
+			ProjectAdmin.AdminModule admin=new ProjectAdmin.AdminModule();
 
 			System.out.println("To continue in the Admin module you must Login for the secure management of the SPICE JET Airlines..");
 
@@ -296,10 +182,15 @@ public class javaproj
 
 			if(AdminStatus==1)
 			{
-				admin.CreateAccount();
+				boolean t;
+
+				t=admin.CreateAccount();
+
+				if(t==true)
+					AdminStatus=2;
 			}
 
-			else if(AdminStatus==2)
+			if(AdminStatus==2)
 			{
 				System.out.println("You must Login to your account to perform the tasks..\n");
 
@@ -311,8 +202,9 @@ public class javaproj
 
 				{
 					System.out.println("Here are the services provided to your Module..\n");
-
-					System.out.println("1.Introduce a new Airline\n2.Change fares of Airlines\n3.Stop the services of certain Airline");
+				while(2>1)
+				{	
+					System.out.println("\n1.Introduce a new Airline\n2.Change fares of Airlines\n3.Stop the services of certain Airline");
 					System.out.println("4.Check daily Transactions\n5.Logout and Exit the Administrative Module\n");
 
 					int AdminService;
@@ -333,37 +225,37 @@ public class javaproj
 					if(AdminService==1)
 					{
 						admin.AddAirline();
-						System.out.println("Your changes are Updated..!!!\n    SPICE JET offers sincere regards to your Services    \n");
-						System.out.println("Have a Great Day...!!\n");
+						System.out.println("Your changes are Updated..!!!\n");
+
 					}
 
 					else if(AdminService==2)
 					{
 						admin.ChangeFare();
-						System.out.println("Your changes are Updated..!!!\n    SPICE JET offers sincere regards to your Services    \n");
-						System.out.println("Have a Great Day...!!\n");
+						System.out.println("Your changes are Updated..!!!\n");
 					}
 
 					else if(AdminService==3)
 					{
 						admin.StopAirline();
-						System.out.println("Your changes are Updated..!!!\n    SPICE JET offers sincere regards to your Services    \n");
-						System.out.println("Have a Great Day...!!\n");
+						System.out.println("Your changes are Updated..!!!\n");
 					}
 
 					else if(AdminService==4)
 					{
 						admin.DailyTransaction();
-						System.out.println("Your changes are Updated..!!!\n    SPICE JET offers sincere regards to your Services    \n");
-						System.out.println("Have a Great Day...!!\n");
+						System.out.println("Your changes are Updated..!!!\n");
 					}
 
 					else if(AdminService==5)
 					{
 						admin.LogoutAccount();
+						System.out.println("    SPICE JET offers sincere regards to your Services    \n");
+						System.out.println("Have a Great Day...!!\n");
+						break;
 					}
 
-
+				}
 
 				}
 
@@ -404,5 +296,5 @@ public class javaproj
 
 		System.out.println("\n\n	SPICE JET  ---	Fastest and Safest....!!!!\n\n");
 	}
-
+	
 }
